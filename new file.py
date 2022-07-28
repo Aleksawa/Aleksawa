17f1d08d0bc33d7fe9b82feafd142b59b6af4d8f
Aleksawa <bushevsantik1988@gmail.com>
2022-07-28 11:59:29 +0300

    new file lesson 2

--- new file.py
+++ new file.py
@@ -1,1 +1,80 @@
-print("Hello Hillel")
+#result = 1 + 1 # sum of two numbers
+#print("result", result)
+#print("Result", type("Resilt"))
+
+
+# number = "2"
+# number_a = 7   #straight method
+# number_b = number_a #reference to another object
+# print("result", number_a,"result", number_b)
+# value: str = 13
+# print((value) type("value"))
+# my_cool_str = "my colour row"
+# my_cool_str_2 = 'this words'
+my_cool_str_3 = '''What we could do like this'''
+# my_cool_str_4 = """What is going on?"""
+# my_cool_str_5 = str(1)
+# my_cool_str_6 = str(5.6)
+# print(my_cool_str)
+# print(my_cool_str_6)
+# print(my_cool_str_3)
+# print(my_cool_str_2)
+
+
+result  = 3 + 2 #sum
+print(result)
+
+result = 3.0 + 2 #sum int + float
+print(result)
+result = 3.0 + 3.0 #sum float + float
+print(result)
+result = 5.1 - 3  #deduction float - int
+print(result)
+# print('result')
+# print(resolut)
+result = 2.1 * 3
+print(result)
+result = 8/4      #divide int / int
+print(result)
+result = 2 ** 3   #exponantiation int ** int
+print(result)
+
+result = 11 // 3
+print(result)
+result = 48 % 10
+print(result)
+
+result = 20 % 4
+print(result)
+
+result = 2 % 4
+print(result)
+
+result = round(3.5)
+print(round)
+
+result = abs(-7)  #value
+print(result)
+result = abs(-7*7)
+print(result)
+
+result = min(1,2,3,4,5,6)
+print(result)
+result = max(1,2,3,4,5,65)
+print(result)
+
+result = 2**3
+print(result)
+result = pow(2,3)
+print(result)
+
+result = round(3.5)
+print(result)
+
+print(my_cool_str_3) #refer to line 14
+
+result = max(1,2,3,4,5,abs(-31))
+print(result)
+
+
+
