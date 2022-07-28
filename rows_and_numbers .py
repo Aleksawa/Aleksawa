824f52330dcad04a9d1e61e0768c87d36d59aa75
Aleksawa <bushevsantik1988@gmail.com>
2022-07-28 15:56:17 +0300

    rows_and_numbers.py 2

--- rows_and_numbers .py
+++ rows_and_numbers .py
@@ -3,3 +3,30 @@ family_name = 'Bushev'
 text = f'''Hello {name} {family_name}''' #text = '''Hello {n} {f}'''. format(n=name,f=family_name)
 print(text)
 
+a = (5.5)
+print((a), type(a))
+
+round(a)      #round(a) = 6
+print(round(a), type(round(a)))
+
+exponantiation = round(a) ** 4
+print(exponantiation, type(exponantiation))
+
+square_root = round(a) ** 0.5
+print((square_root), type (square_root))
+
+division_remainder = round(a) % 2
+print((division_remainder), type(division_remainder))
+
+
+
+
+
+
+
+
+
+
+
+
+
