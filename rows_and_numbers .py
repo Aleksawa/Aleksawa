b88e55d1527208d3e7dd059614d7fab4dac26771
Aleksawa <bushevsantik1988@gmail.com>
2022-07-31 13:24:12 +0300

    rows_and_numbers.py 2 rows_and_numbers_advance.py

--- rows_and_numbers .py
+++ rows_and_numbers .py
@@ -2,21 +2,38 @@ name = 'Aleksandr'
 family_name = 'Bushev'
 text = f'''Hello {name} {family_name}''' #text = '''Hello {n} {f}'''. format(n=name,f=family_name)
 print(text)
+print(len(name), len(family_name))
 
 a = (5.5)
 print((a), type(a))
 
-round(a)      #round(a) = 6
-print(round(a), type(round(a)))
+value_1 = int(a)
+print(value_1, type(value_1))
 
-exponantiation = round(a) ** 4
+exponantiation = value_1 ** 4
 print(exponantiation, type(exponantiation))
 
-square_root = round(a) ** 0.5
+square_root = value_1 ** 0.5
 print((square_root), type (square_root))
 
-division_remainder = round(a) % 2
+division_remainder = value_1 % 2
 print((division_remainder), type(division_remainder))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,14 @@
+name = " wiLLiam "
+a = name.strip()
+print(a)
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.lower(), a.upper())
+print(f'''Hello {a.capitalize()}''')
+print(len(a))
+print(a[::-1])
+
+
+
+
