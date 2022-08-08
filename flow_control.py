4dbe457b57a81ca4457248efd050709183985a9b
Lenovo T440p <bushevsantik1988@gmail.com>
2022-08-08 22:18:34 +0300

    Lesson 4 homework

--- flow_control.py
+++ flow_control.py
@@ -1,17 +1,21 @@
-operand_1 = (input("Enter first data "))
-operand_2 = (input("What calculation you like to do? "))
-operand_3 = (input("Enter your second data "))
+num_1 = (input("Enter first data "))
+calculation = (input("What calculation you like to do? "))
+num_2 = (input("Enter your second data "))
 
-operand_1 = int(operand_1) if operand_1.isdigit() else float(operand_1)
-operand_3 = int(operand_3) if operand_3.isdigit() else float(operand_3)
+num_1 = int(num_1) if num_1.isdigit() else float(num_1)
+num_2 = int(num_2) if num_2.isdigit() else float(num_2)
 
-if operand_2 == '-':
-    print(operand_1 - operand_3, type(operand_1 - operand_3))
-elif operand_2 == '+':
-    print(operand_1 + operand_3,  type(operand_1 - operand_3))
-elif operand_2 == '/':
-    print(operand_1/operand_3,  type(operand_1 - operand_3))
-elif operand_2 == '*':
-    print(operand_1 * operand_3,  type(operand_1 - operand_3))
+if calculation == '-':
+    print(num_1 - num_2, type(num_1 - num_2))
+elif calculation == '+':
+    print(num_1 + num_2,  type(num_1 + num_2))
+elif calculation == '/':
+    if num_2 == 0:
+        print("ZeroDivisionError")
+    else:
+        answer = num_1 / num_2
+    print(num_1/num_2,  type(num_1/num_2))
+elif calculation == '*':
+    print(num_1 * num_2,  type(num_1 * num_2))
 else:
     print("Error")
