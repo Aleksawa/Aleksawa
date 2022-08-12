157136cf2f43c5f79875b25bf36da02f5ccad1ff
Lenovo T440p <bushevsantik1988@gmail.com>
2022-08-12 16:07:53 +0300

    Lesson 5 homework

--- flow_control.py
+++ flow_control.py
@@ -1,7 +1,13 @@
+print ("Exit from program press enter e ")
+
 while True:
     num_1 = (input("Enter first data "))
+    if num_1 == 'e':
+        break
     calculation = (input("What calculation you like to do? "))
     num_2 = (input("Enter your second data "))
+
+
 
     num_1 = int(num_1) if num_1.isdigit() else float(num_1)
     num_2 = int(num_2) if num_2.isdigit() else float(num_2)
@@ -9,19 +15,57 @@ while True:
     if calculation == '-':
         print(num_1 - num_2, type(num_1 - num_2))
         continue
+
     elif calculation == '+':
         print(num_1 + num_2,  type(num_1 + num_2))
         continue
+
     elif calculation == '/':
         if num_2 == 0:
             print("ZeroDivisionError")
             continue
-        else:answer = num_1 / num_2
+
+        else:
+            answer = num_1 / num_2
         print(num_1/num_2,  type(num_1/num_2))
         continue
+
     elif calculation == '*':
         print(num_1 * num_2,  type(num_1 * num_2))
         continue
     else:
         print("Error")
 
+
+
+
+
+
+
+# бесконечный цикл
+# while True:
+#     num_1 = (input("Enter first data "))
+#     calculation = (input("What calculation you like to do? "))
+#     num_2 = (input("Enter your second data "))
+#
+#     num_1 = int(num_1) if num_1.isdigit() else float(num_1)
+#     num_2 = int(num_2) if num_2.isdigit() else float(num_2)
+#
+#     if calculation == '-':
+#         print(num_1 - num_2, type(num_1 - num_2))
+#         continue
+#     elif calculation == '+':
+#         print(num_1 + num_2,  type(num_1 + num_2))
+#         continue
+#     elif calculation == '/':
+#         if num_2 == 0:
+#             print("ZeroDivisionError")
+#             continue
+#         else:answer = num_1 / num_2
+#         print(num_1/num_2,  type(num_1/num_2))
+#         continue
+#     elif calculation == '*':
+#         print(num_1 * num_2,  type(num_1 * num_2))
+#         continue
+#     else:
+#         print("Error")
